651b9487c1467a5be5aa44726a6b4d1f606dcc70
Santo Cariotti <santo@dcariotti.me>
2021-03-16 14:17:45 +0100

    feat: create and search email

--- src/email/models.rs
+++ src/email/models.rs
@@ -1,10 +1,13 @@
 use crate::db::get_client;
-use crate::errors::AppError;
+use crate::errors::{AppError, AppErrorType};
 
 use deadpool_postgres::Pool;
 use serde::{Deserialize, Serialize};
 use tokio_pg_mapper::FromTokioPostgresRow;
 use tokio_pg_mapper_derive::PostgresMapper;
+
+use hex;
+use md5::{Digest, Md5};
 
 #[derive(Serialize, Deserialize, PostgresMapper)]
 #[pg_mapper(table = "email")]
@@ -34,5 +37,75 @@ impl Email {
             .collect::<Vec<Email>>();
 
         Ok(emails)
+    }
+
+    /// Search an email
+    pub async fn search(
+        pool: Pool,
+        email: &String,
+    ) -> Result<Email, AppError> {
+        let client = get_client(pool.clone()).await.unwrap();
+
+        let statement =
+            client.prepare("SELECT * FROM email WHERE email=$1").await?;
+
+        let email = client
+            .query_opt(&statement, &[&email])
+            .await?
+            .map(|row| Email::from_row_ref(&row).unwrap());
+
+        match email {
+            Some(email) => Ok(email),
+            None => Err(AppError {
+                error_type: AppErrorType::NotFoundError,
+                cause: None,
+                message: Some("Email not found".to_string()),
+            }),
+        }
+    }
+
+    /// Create new email
+    pub async fn create(
+        pool: Pool,
+        data: &EmailData,
+    ) -> Result<Email, AppError> {
+        // Search an email  that matches with that string, because if it's
+        // exists, the server cannot create a clone
+        match Email::search(pool.clone(), &data.email).await {
+            Ok(_) => {
+                return Err(AppError {
+                    message: Some("Email already exists".to_string()),
+                    cause: Some("".to_string()),
+                    error_type: AppErrorType::AuthorizationError,
+                });
+            }
+            Err(_) => {}
+        };
+
+        let client = get_client(pool.clone()).await.unwrap();
+
+        let mut hasher = Md5::new();
+        hasher.update(&data.email.as_bytes());
+        let hash_final = hasher.finalize();
+
+        let digest = hex::encode(&hash_final.as_slice());
+
+        let statement = client
+            .prepare("INSERT INTO email VALUES ($1, $2) RETURNING *")
+            .await?;
+
+        let email = client
+            .query_opt(&statement, &[&data.email, &digest])
+            .await?
+            .map(|row| Email::from_row_ref(&row).unwrap());
+
+        match email {
+            Some(email) => Ok(email),
+            None => Err(AppError {
+                message: Some("Error creating a new email".to_string()),
+                cause: Some("Unknown error".to_string()),
+                error_type: AppErrorType::DbError,
+            }),
+        }
     }
 }
